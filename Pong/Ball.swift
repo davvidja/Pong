1668e885f60575c9f24b959bb42fc31689a407ee
Jaume Figuerola <jaumefm@iMac-de-Jaume-Figuerola.local>
2014-11-29 11:49:26 +0100

    New Ball Class added. GameScene.sks Deleted. Working with touches.

---
+++ Pong/Ball.swift
@@ -0,0 +1,15 @@
+//
+//  Ball.swift
+//  Pong
+//
+//  Created by Jaume Figuerola on 29/11/14.
+//  Copyright (c) 2014 Our company. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Ball: SKSpriteNode {
+    
+    
+}
--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -8,34 +8,41 @@
 
 import SpriteKit
 
+var ball : Ball = Ball()
+var ballTexture = SKTexture(imageNamed: "Spaceship")
+
+
 class GameScene: SKScene {
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
         let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
-        
+        myLabel.text = "Hello, World!"
+        myLabel.fontSize = 65
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
         self.addChild(myLabel)
+        
+        /* Adding the Ball to the Scene */
+        ball = Ball(texture: ballTexture)
+        ball.position =  CGPoint(x: self.size.width / 2, y: self.size.height / 2)
+        
+        let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+        ball.runAction(SKAction.repeatActionForever(action))
+        self.addChild(ball)
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-            let location = touch.locationInNode(self)
+            var location = touch.locationInNode(self)
+            var node = nodeAtPoint(location)
             
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            if (node == ball) {
+                ball.physicsBody = SKPhysicsBody(texture: ballTexture, size: CGSize(width: 15,height: 15))
+                NSLog("Ha detectat!!")
+            }
             
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
         }
     }
    
