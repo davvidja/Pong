647aad229635fe53cffcc3d7399328d7b8265eed
Jaume Figuerola <jaumefm@iMac-de-Jaume-Figuerola.local>
2014-11-29 10:50:23 +0100

    Added Navigation View Controller.

---
+++ Pong/ChooseRolViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChooseRolViewController.swift
+//  Pong
+//
+//  Created by Jaume Figuerola on 29/11/14.
+//  Copyright (c) 2014 Our company. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class  ChooseRolViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.navigationBarHidden = true
+        self.navigationController?.toolbarHidden = true
+        // self.setNavigationBarHidden(true,animated: false)
+        // self.setToolbarHidden(true,animated:false)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
